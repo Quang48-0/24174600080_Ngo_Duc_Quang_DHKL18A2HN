dc289026171048f76a84752048ea839c414f0e70
Ngo Duc Quang <ndquang.24174600080@sv.uneti.edu.vn>
2024-10-16 19:49:43 +0700

    Initial commit - Add hello.py

---
+++ chương_6/hello.py
@@ -0,0 +1,12 @@
+# Mỗi dòng tương ứng với một hàng của chữ "Hello"
+hello = [
+    "H   H  EEEEE  L      L       OOO  ",
+    "H   H  E      L      L      O   O ",
+    "HHHHH  EEEE   L      L      O   O ",
+    "H   H  E      L      L      O   O ",
+    "H   H  EEEEE  LLLLL  LLLLL   OOO  "
+]
+
+# In từng dòng 
+for line in hello:
+    print(line)
